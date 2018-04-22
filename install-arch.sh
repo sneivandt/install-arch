460c23004507501f396add767a657419f91a63ce
sneivandt <sneivandt@users.noreply.github.com>
2018-04-22 16:49:55 -0700

    LVM on LUKS

--- install-arch.sh
+++ install-arch.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-# TODO:
-#   - LUKS on LVM
-#   - Disk encryption
-#   - Install AUR packages
-#   - Accept user input to decide if GUI will be installed
-#   - Accept user input to decide if virtualbox guest additions will be installed
-#   - Accept user input to decide what video drivers will be installed
+# Preamble ---------------------------------------------------------------- {{{
+#
+# Prepare for execution
 
-# Trap Error
+# Error Trap
 set -uo pipefail
 trap 'echo "$0: Error on line "$LINENO": $BASH_COMMAND"' ERR
 
 # User Input
-hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
-: "${hostname:?"hostname cannot be empty"}"
-user=$(dialog --stdout --inputbox "Enter username" 0 0) || exit 1
-: "${user:?"user cannot be empty"}"
-password=$(dialog --stdout --passwordbox "Enter password" 0 0) || exit 1
-: "${password:?"password cannot be empty"}"
-password2=$(dialog --stdout --passwordbox "Enter password again" 0 0) || exit 1
-[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+hostname=$(dialog --stdout --clear --inputbox "Enter hostname" 0 40) || exit 1
+[ -z "$hostname" ] && echo "hostname cannot be empty" && exit 1
+user=$(dialog --stdout --clear --inputbox "Enter username" 0 40) || exit 1
+[ -z "$user" ] && echo "username cannot be empty" && exit 1
+password1=$(dialog --stdout --clear --insecure --passwordbox "Enter password" 0 40) || exit 1
+[ -z "$password1" ] && echo "password cannot be empty" && exit 1
+password2=$(dialog --stdout --clear --insecure --passwordbox "Enter password again" 0 40) || exit 1
+[[ "$password1" == "$password2" ]] || ( echo "Passwords did not match" && exit 1 )
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
+device=$(dialog --stdout --clear --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
+password_luks1=$(dialog --stdout --clear --insecure --passwordbox "Enter disk encryption password" 0 40) || exit 1
+[ -z "$password_luks1" ] && echo "disk encryption password cannot be empty" && exit 1
+password_luks2=$(dialog --stdout --clear --insecure --passwordbox "Enter disk encryption password again" 0 40) || exit 1
+[[ "$password_luks1" == "$password_luks2" ]] || ( echo "Passwords did not match" && exit 1 )
 
 # Logging
 exec 1> >(tee "stdout.log")
 exec 2> >(tee "stderr.log")
 
-# Enable ntp
-timedatectl set-ntp true
+# }}}
+# Disks ------------------------------------------------------------------- {{{
+#
+# Setup the disks
 
 # Partitioning
 fdisk "$device" <<'EOF'
@@ -43,33 +45,44 @@ n
 p
 2
 
-+4096M
+
 t
 2
-82
-n
-p
-3
-
-
+8e
 w
 EOF
 
-# Wipe Drives
-wipefs "$device"1
-wipefs "$device"2
-wipefs "$device"3
+# Encrypt Root Drive
+echo -n "$password_luks1" | cryptsetup luksFormat --type luks2 "$device"2 -
 
-# Make Filesystem
+# Open Root Drive
+echo -n "$password_luks1" | cryptsetup open "$device"2 cryptlvm -
+
+# Create Physical Volume
+pvcreate /dev/mapper/cryptlvm
+
+# Create Volume Group
+vgcreate volgroup0 /dev/mapper/cryptlvm
+
+# Create Logical Volumes
+lvcreate -L 1G volgroup0 -n swap
+lvcreate -l 100%FREE volgroup0 -n root
+
+# Fromat Filesystems
+mkswap /dev/mapper/volgroup0-swap
+mkfs.ext4 /dev/mapper/volgroup0-root
 mkfs.ext2 "$device"1
-mkswap "$device"2
-mkfs.ext4 "$device"3
 
-# Mount Drives
-swapon "$device"2
-mount "$device"3 /mnt
+# Mount Filesystems
+mount /dev/mapper/volgroup0-root /mnt
+swapon /dev/mapper/volgroup0-swap
 mkdir /mnt/boot
 mount "$device"1 /mnt/boot
+
+# }}}
+# Pacstrap ---------------------------------------------------------------- {{{
+#
+# Install packages
 
 # Update Mirrors
 curl -s 'https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4' > /etc/pacman.d/mirrorlist
@@ -81,7 +94,46 @@ cat >>/etc/pacman.conf <<'EOF'
 SigLevel = Never
 Server = http://repo.archlinux.fr/$arch
 EOF
-pacstrap /mnt adobe-source-code-pro-fonts base base-devel compton ctags curl dmenu dunst feh git grub i3-gaps ntp openssh rxvt-unicode playerctl redshift tmux vim wget xautolock xf86-video-vesa xorg xorg-server xorg-xinit xterm yaourt zip zsh
+pacstrap /mnt \
+  adobe-source-code-pro-fonts \
+  base \
+  base-devel \
+  compton \
+  ctags \
+  curl \
+  dmenu \
+  dunst \
+  feh \
+  git \
+  grub \
+  i3-gaps \
+  ntp \
+  openssh \
+  rxvt-unicode \
+  playerctl \
+  redshift \
+  thunar \
+  tmux \
+  vim \
+  wget \
+  xautolock \
+  xf86-video-vesa \
+  xorg \
+  xorg-server \
+  xorg-xinit \
+  xterm \
+  yaourt \
+  zip \
+  zsh
+
+# TODO: Accept user input to decide if xorg/GUI applicaitons will be installed
+# TODO: Accept user input to decide if virtualbox guest additions will be installed
+# TODO: Accept user input to decide what video drivers will be installed
+
+# }}}
+# General ----------------------------------------------------------------- {{{
+#
+# General system config
 
 # Generate Filesystem Table
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -104,33 +156,61 @@ arch-chroot /mnt systemctl enable dhcpcd
 # Enable ntpd
 arch-chroot /mnt systemctl enable ntpd
 
-# Set timezone
-arch chroot /mnt ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+# Set Time Zone
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+
+# }}}
+# AUR --------------------------------------------------------------------- {{{
+#
+# Install AUR packages
+
+cat >>/mnt/etc/pacman.conf <<'EOF'
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/$arch
+EOF
+# TODO: Use yaourt to install AUR packages
+
+# }}}
+# Users  ------------------------------------------------------------------ {{{
+#
+# Configure users
 
 # Create User
 arch-chroot /mnt useradd -mU -G wheel -s /usr/bin/zsh "$user"
 arch-chroot /mnt chsh -s /usr/bin/zsh "$user"
 arch-chroot /mnt sed -i '/^# %wheel ALL=(ALL) ALL$/s/^# //g' /etc/sudoers
 
-# Set Passwords
-echo "$user:$password" | chpasswd --root /mnt
-echo "root:$password" | chpasswd --root /mnt
-
-# AUR packages
-cat >>/mnt/etc/pacman.conf <<'EOF'
-[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/$arch
-EOF
-
-# Install dotfiles
+# Install sneivandt/dotfiles
 arch-chroot /mnt su "$user" -c "git clone https://github.com/sneivandt/dotfiles.git /home/$user/src/dotfiles"
 arch-chroot /mnt su "$user" -c "/home/$user/src/dotfiles/dotfiles.sh install --gui"
+
+# Set Passwords
+echo "root:$password1" | chpasswd --root /mnt
+echo "$user:$password1" | chpasswd --root /mnt
+
+# }}}
+# Init -------------------------------------------------------------------- {{{
+#
+# Configure system startup
+
+# Create init ramdisk
+sed -i "s/^HOOKS.*/HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)/" /mnt/etc/mkinitcpio.conf
+arch-chroot /mnt mkinitcpio -p linux
 
 # Install Grub
 arch-chroot /mnt grub-install "$device"
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+device_esc=$(sed 's/\//\\\//g' <<< "$device")
+sed -i "s/.*vmlinuz-linux.*/linux \\/vmlinuz-linux root=\\/dev\\/mapper\\/volgroup0-root rw cryptdevice=${device_esc}2:volgroup0 quiet/" /mnt/boot/grub/grub.cfg
 
-# Cleanup
+# }}}
+# Cleanup ----------------------------------------------------------------- {{{
+#
+# Complete setup
+
+# Release resources
 umount -R /mnt
 swapoff -a
+
+# }}}
